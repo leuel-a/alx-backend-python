473200d74d6bc804cb1112941c9fd6805424c691
nefelibatala1993 <leuela1993@gmail.com>
2023-08-03 08:55:27 +0300

    add README and finish first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Defines a function that adds two numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two numbers and returns their result
+
+    Parameters:
+    a (float): first number
+    b (float): second number
+
+    Returns:
+    int: the sum of the two numbers
+    """
+    return a + b
