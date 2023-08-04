fcfce63f38fa51f4412bca5a01a9a1f8fdbd008d
nefelibatala1993 <leuela1993@gmail.com>
2023-08-04 19:24:51 +0300

    second task

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,15 @@
-#!/usr
+#!/usr/bin/env python3
+"""Defines the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns their result
+
+    Parameters:
+    str1 (str): the first string
+    str2 (str): the second string
+
+    Returns:
+    str: the concatenation of the two strings
+    """
+    return str1 + str2
