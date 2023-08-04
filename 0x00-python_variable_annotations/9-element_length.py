177b792bb0ba5b90a703f3250bfde49f300c85b7
nefelibatala1993 <leuela1993@gmail.com>
2023-08-04 21:47:20 +0300

    attempt fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""Defines the element_length function"""
-from typing import Sequence, Iterable, List, Tuple
+""" Let's duck type an iterable object"""
+from typing import Mapping, MutableMapping, Sequence, Iterable, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[SyntaxWarning, int]]:
-    """Returns the elements inside the itereable, where each value is the
-    element it self and its length"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Element length """
     return [(i, len(i)) for i in lst]
