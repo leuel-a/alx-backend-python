c7077d25c981b0004c16d5fbe1783b78ba7e4ca1
nefelibatala1993 <leuela1993@gmail.com>
2023-08-04 21:45:56 +0300

    pass the pycodestyle check

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,9 +3,12 @@
 from typing import Any, Mapping, TypeVar, Union
 
 
-def safely_get_value(dct: Mapping, key: Any,
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
         default: Union[TypeVar('T'), None]) -> Union[Any, TypeVar('T')]:
-    """Return the value linked to key in dct if exists, otherwise return default"""
+    """Return the value linked to key in dct if exists,
+    otherwise return default"""
     if key in dct:
         return dct[key]
     else:
