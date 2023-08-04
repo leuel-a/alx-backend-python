f7616deb3aafd85813bdc222edc8db97c9b56114
nefelibatala1993 <leuela1993@gmail.com>
2023-08-04 21:35:03 +0300

    add the last advanced task

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the safely_get_value function"""
+from typing import Any, Mapping, TypeVar, Union
+
+
+def safely_get_value(dct: Mapping, key: Any,
+        default: Union[TypeVar('T'), None]) -> Union[Any, TypeVar('T')]:
+    """Return the value linked to key in dct if exists, otherwise return default"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
