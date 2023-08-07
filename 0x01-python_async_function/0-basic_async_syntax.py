d006bf04c8568ce654cd83a38e3f93c864587529
nefelibatala1993 <leuela1993@gmail.com>
2023-08-07 22:12:42 +0300

    first task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This is the first task of the Python - Async project"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """This function waits a random value and returns its value"""
+    random_number: float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
