93ee6afd4286e85e39a37d21db99e26e6a27bbb6
leuel-a <leuela1993@gmail.com>
2024-04-10 13:05:19 +0300

    fix: make the function a plain function instead of an async function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,9 +6,14 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """Measures the runtime of wait_n"""
+def measure_time(n: int, max_delay: int) -> float:
+    """Measures the total execution time for wait_n(n, max_delay)"""
     start_time = time.time()
-    await wait_n(n, max_delay)
+
+    # The only way to run an async function is to use asyncio.run() in python
+    asyncio.run(wait_n(n, max_delay))
+
     end_time = time.time()
-    return (end_time - start_time) / n
+
+    total_time = end_time - start_time
+    return total_time / n
