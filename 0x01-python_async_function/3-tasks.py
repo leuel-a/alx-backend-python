d6cb2df6fa8e9eb790a9c6b73707f15cca837a95
leuel-a <leuela1993@gmail.com>
2024-04-10 13:18:58 +0300

    add: the fourth task of the project finished

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines the task_wait_random function"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates a task that will be executed later on for the wait_random function"""
+    return asyncio.create_task(wait_random(max_delay))
