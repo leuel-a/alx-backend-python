33f3d05f883079140671d8284bb491c6edae3863
leuel-a <leuela1993@gmail.com>
2024-04-10 13:21:54 +0300

    fix: pass pycodestyle check

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,5 +6,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Creates a task that will be executed later on for the wait_random function"""
+    """Creates a task that will be executed later on for the
+    wait_random function"""
     return asyncio.create_task(wait_random(max_delay))
